be10ca7880d50d1e6b0805e3b244ea4b47b1872e
ralifian69-prog <ralifian69@gmail.com>
2026-09-01 00:58:58 +0700

    Refactor ModuleMain to enhance logging on load

--- app/src/main/java/com/ralifian69/vibrationhook/ModuleMain.java
+++ app/src/main/java/com/ralifian69/vibrationhook/ModuleMain.java
@@ -1,21 +1,48 @@
 package com.ralifian69.vibrationhook;
 
+import android.util.Log;
+
 import io.github.libxposed.api.XposedModule;
+import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam;
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam;
 
 public class ModuleMain extends XposedModule {
 
+    private static final String TARGET_PACKAGE =
+            "com.FishingPlanetLL.FishingPlanet";
+
     @Override
-    public void onModuleLoaded() {
-        log(Log.INFO, "VibrationHook", "Module berhasil dimuat");
+    public void onModuleLoaded(ModuleLoadedParam param) {
+        log(
+                Log.INFO,
+                "VibrationHook",
+                "Module berhasil dimuat: " + param.getProcessName()
+        );
     }
 
     @Override
     public void onPackageLoaded(PackageLoadedParam param) {
-        if (!"com.FishingPlanetLLC.FishingPlanet".equals(param.getPackageName())) {
+
+        if (!TARGET_PACKAGE.equals(param.getPackageName())) {
             return;
         }
 
-        log(Log.INFO, "VibrationHook",
-                "Fishing Planet terdeteksi: " + param.getPackageName());
+        log(
+                Log.INFO,
+                "VibrationHook",
+                "Fishing Planet ditemukan!"
+        );
+
+        log(
+                Log.INFO,
+                "VibrationHook",
+                "Process: " + param.getProcessName()
+        );
+
+        log(
+                Log.INFO,
+                "VibrationHook",
+                "ClassLoader: " + param.getDefaultClassLoader()
+        );
     }
 }
