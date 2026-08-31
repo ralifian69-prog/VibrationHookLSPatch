eec1d9a3e572e0aee916b041b3372b67d1eb03cd
ralifian69-prog <ralifian69@gmail.com>
2026-09-01 01:08:48 +0700

    Refactor logging in onPackageLoaded method

--- app/src/main/java/com/ralifian69/vibrationhook/ModuleMain.java
+++ app/src/main/java/com/ralifian69/vibrationhook/ModuleMain.java
@@ -3,22 +3,13 @@ package com.ralifian69.vibrationhook;
 import android.util.Log;
 
 import io.github.libxposed.api.XposedModule;
-import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam;
 import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam;
 
 public class ModuleMain extends XposedModule {
 
+    private static final String TAG = "VibrationHook";
     private static final String TARGET_PACKAGE =
             "com.FishingPlanetLL.FishingPlanet";
-
-    @Override
-    public void onModuleLoaded(ModuleLoadedParam param) {
-        log(
-                Log.INFO,
-                "VibrationHook",
-                "Module berhasil dimuat: " + param.getProcessName()
-        );
-    }
 
     @Override
     public void onPackageLoaded(PackageLoadedParam param) {
@@ -27,22 +18,8 @@ public class ModuleMain extends XposedModule {
             return;
         }
 
-        log(
-                Log.INFO,
-                "VibrationHook",
-                "Fishing Planet ditemukan!"
-        );
-
-        log(
-                Log.INFO,
-                "VibrationHook",
-                "Process: " + param.getProcessName()
-        );
-
-        log(
-                Log.INFO,
-                "VibrationHook",
-                "ClassLoader: " + param.getDefaultClassLoader()
-        );
+        Log.i(TAG, "Fishing Planet ditemukan!");
+        Log.i(TAG, "Process: " + param.getProcessName());
+        Log.i(TAG, "ClassLoader: " + param.getDefaultClassLoader());
     }
 }
